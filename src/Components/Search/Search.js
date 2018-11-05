c80e226f19a5e0bffc627ee159f2053204527526
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-11-04 21:28:22 -0700

    finish dialog and img results

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -15,11 +15,16 @@ class Search extends Component {
   };
 
   textCaptureHandler = (e) => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&
       image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-        .then(res => this.setState({ images: res.data.hits }))
-        .catch(err => console.log(err))
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err))
+      }
     })
   };
 
@@ -51,8 +56,8 @@ class Search extends Component {
           <MenuItem value={30} primaryText="30" />
           <MenuItem value={50} primaryText="50" />
         </SelectField>
-        <br/>
-        {this.state.images.length > 0 ? (<ImgResults images={this.state.images}/>) : null }
+        <br />
+        {this.state.images.length > 0 ? (<ImgResults images={this.state.images} />) : null}
       </div>
     )
   }
